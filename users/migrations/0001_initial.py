9ffa054d547053b51a657dbaaf42ebcc8505ec62
patrykbakowski <patryk.bakowski@gmail.com>
2026-09-25 23:21:27 +0200

    fix: align initial user migration metadata

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
                 ("groups", models.ManyToManyField(blank=True, help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.", related_name="user_set", related_query_name="user", to="auth.group", verbose_name="groups")),
                 ("user_permissions", models.ManyToManyField(blank=True, help_text="Specific permissions for this user.", related_name="user_set", related_query_name="user", to="auth.permission", verbose_name="user permissions")),
             ],
-            options={"abstract": False},
+            options={"verbose_name": "user", "verbose_name_plural": "users", "abstract": False},
             managers=[("objects", django.contrib.auth.models.UserManager())],
         ),
     ]

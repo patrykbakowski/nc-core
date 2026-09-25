700452588b582ef36aaffdce80d30807cb8af16b
patrykbakowski <patryk.bakowski@gmail.com>
2026-09-25 23:22:36 +0200

    security: enforce active QM account on API

--- qm_core/settings.py
+++ qm_core/settings.py
@@ -98,6 +98,6 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        "api.permissions.IsActiveQMUser",
     ],
 }

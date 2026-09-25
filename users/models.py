250950ed08dd95ac7aedfce5d345312e1b94e8a8
patrykbakowski <patryk.bakowski@gmail.com>
2026-09-25 23:10:26 +0200

    stage1: add users/models.py

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    class Status(models.TextChoices):
+        ACTIVE = "active", "Active"
+        SUSPENDED = "suspended", "Suspended"
+        DELETED = "deleted", "Deleted"
+
+    email = models.EmailField(unique=True)
+    status = models.CharField(max_length=16, choices=Status.choices, default=Status.ACTIVE)
+
+    def __str__(self):
+        return self.email
